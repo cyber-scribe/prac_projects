ed722e55cee2b34e0619acde45aa49b54d80cbb6
cyber-scribe <vd297520@gmail.com>
2025-04-11 15:12:32 +0530

    changes

--- new_project/app.py
+++ new_project/app.py
@@ -1,9 +1,9 @@
 import requests;
 import numpy as np;
-# import pandas as pd;
+import pandas as pd;
 import joblib;
 import tensorflow as tf;
-# from tensorflow.keras.models import load_model;
+from tensorflow.keras.models import load_model;
 from flask import Flask, request, jsonify;
 
 
