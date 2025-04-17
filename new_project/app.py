613258272095a30b7cb2a9ae7747ffd676e7d9bc
cyber-scribe <vd297520@gmail.com>
2025-04-17 17:08:30 +0530

    changes

--- new_project/app.py
+++ new_project/app.py
@@ -9,8 +9,8 @@ from flask import Flask, request, jsonify;
 
 
 def get_real_time_weather(city):
-    API_KEY = "YOUR_OPENWEATHER_API_KEY" 
-    URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    API_KEY = "" 
+    # URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
     try:
         response = requests.get(URL)
