d54b792b3c5b5b9ae0d3f4836e5aacd113143203
cyber-scribe <vd297520@gmail.com>
2025-07-13 19:35:40 +0530

    changes

--- CS_projects/Password_Analyzer/script.js
+++ CS_projects/Password_Analyzer/script.js
@@ -1,3 +1,10 @@
 const passwordInput = document.getElementById("password");
 const scoreDiv = document.getElementById("score");
 const feedbackDiv = document.getElementById("fdbck");
+
+passwordInput.addEventListener("input", () => {
+  const pw = passwordInput.value;
+  const result = zxcvbn(pw);
+
+  
+});
